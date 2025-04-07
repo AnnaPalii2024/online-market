669edfdfdb44b82a8f8d9713cc168a128e2eec63
AnnaPalii <mspaliy0@gmail.com>
2025-04-07 14:20:59 +0200

    task2

--- store/models.py
+++ store/models.py
@@ -9,3 +9,10 @@ class Category(models.Model):
     class Meta:
         ordering = ['name']
         verbose_name_plural = 'Categories'
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    contact_email = models.EmailField(max_length=75,unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    def __str__(self):
+        return self.name
