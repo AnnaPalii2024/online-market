827e2f7241d717f3da035876a2d4008d238270d6
svetlanapostel <svdzalaty33@gmail.com>
2025-04-28 09:19:26 +0200

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # Поля, которые будут отображаться в списке записей
+    list_display = ('name',)
+
+    # Добавление поиска по полю 'name'
+    search_fields = ('name',)
+
+    # Сортировка записей по полю 'name' в алфавитном порядке
+    ordering = ('name',)
+
